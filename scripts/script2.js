7f024f6d126ca364af9d22fda64dbd643fa141c3
Neoob <gabrielngc@gmail.com>
2022-10-04 12:49:00 -0300

    preenchendo pizza area

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,4 +1,5 @@
 const c = (el) => document.querySelector(el);
+const cl = (els) => document.querySelector(els);
 
 const pizzaData = async function () {
   try {
@@ -20,6 +21,24 @@ const pizzaData = async function () {
       pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizzaDescription;
       console.log(pizzaItem);
       pizzaItem.querySelector('.pizza-item--name').innerHTML = pizzaName;
+
+      // pizzaWindow
+      pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        // ligando pizza area
+        e.preventDefault();
+        const pizzaWindowArea = c('.pizzaWindowArea');
+        pizzaWindowArea.style.opacity = 0;
+        pizzaWindowArea.style.display = 'flex';
+        setTimeout(() => {
+          pizzaWindowArea.style.opacity = 1;
+        }, 150);
+
+        // preenchendo pizza area
+        pizzaWindowArea.querySelector('.pizzaInfo h1').innerHTML = pizzaName;
+        pizzaWindowArea.querySelector('.pizzaBig img').src = pizzaImg;
+        pizzaWindowArea.querySelector('.pizzaInfo--desc').innerHTML = pizzaDescription;
+        pizzaWindowArea.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaPrice.toString().replace('.', ',')}`
+      })
 
       c('.pizza-area').appendChild(pizzaItem);
 
